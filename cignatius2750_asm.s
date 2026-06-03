40b144af8b78cdb731dd7b77dae3157e3c00d331
chinonso praise ignatius <cignatius2750@conestogac.on.ca>
2026-06-03 18:58:06 -0400

    Added a busy delay function

--- cignatius2750_asm.s
+++ cignatius2750_asm.s
@@ -20,4 +20,30 @@ cignatius2750_add_test:
     bx lr
     .size   cignatius2750_add_test, .-cignatius2750_add_test
 
+
+@ Function Declaration : int busy_delay(int cycles)
+@
+@ Input: r0 (i.e. r0 holds number of cycles to delay)
+@ Returns: r0
+@ 
+
+@ Here is the actual function. DO NOT MODIFY THIS FUNCTION.
+busy_delay:
+
+    push {r6}
+
+    mov r6, r0
+
+delay_label:
+    subs r6, r6, #1
+
+    bge delay_label
+
+    mov r0, #0                      @ Always return zero (success)
+
+    pop {r6}
+
+    bx lr                           @ Return (Branch eXchange) to the address in the link register (lr)
+
+
 .end
